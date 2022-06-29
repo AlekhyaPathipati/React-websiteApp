5a88843258cbc6d026b15d6c1c89fc9e2e5f6945
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-06-29 19:53:20 +0530

    Add files via upload

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+return (
+	<div>
+	<h1>
+    React is an excellent tool with which to create interactive applications for mobile, web, and other platforms. 
+    React's popularity and usage are increasing day by day for good reason. 
+    As a developer, coding in React makes you better at JavaScript, a language that holds nearly 90% of the web development share today.
+	</h1>
+	</div>
+);
+};
+
+export default About;
---
+++ src/pages/blogs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogs = () => {
+return (
+	<h1>React is one of the most wide-spread UI libraries for web app development. 
+        It is simple, efficient, and has an extensive structure of components and support libraries. 
+        It is precisely this complexity that makes learning React particularly daunting to learn.</h1>
+);
+};
+
+export default Blogs;
